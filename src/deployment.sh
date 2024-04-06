90989ac5ff294b5d4d3957321967dc6036f3de37
avidito <av.leonardif@gmail.com>
2024-04-06 16:09:31 +0700

    feat: update deployment script

---
+++ src/deployment.sh
@@ -0,0 +1,5 @@
+docker build -t puska-farm-dwh .
+
+docker stop puska-farm-dwh
+docker rm puska-farm-dwh
+docker run --name puska-farm-dwh -p 5601:5432 --env-file .env -v puska-farm-dwh-volume:/var/lib/postgresql --restart always -d puska-farm-dwh
