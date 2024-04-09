dba812707dc199d717630fe59abc25ee6a37f3fe
Leonardi Fabianto <29391835+avidito@users.noreply.github.com>
2024-04-09 10:58:58 +0700

    feat: adding new table log_batch (#1)

---
+++ src/ddl/log_batch.sql
@@ -0,0 +1,10 @@
+CREATE TABLE log_batch (
+    table_name VARCHAR(100),
+    params TEXT,
+    processed_rows INTEGER,
+    start_tm TIMESTAMP,
+    end_tm TIMESTAMP,
+    duration FLOAT,
+    created_dt TIMESTAMP,
+    modified_dt TIMESTAMP
+);
