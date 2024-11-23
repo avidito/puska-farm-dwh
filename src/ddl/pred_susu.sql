9e57740e5cb51e6fdc60beb8e3c4b47f3c5f187f
avidito <av.leonardif@gmail.com>
2024-11-23 17:55:56 +0700

    feat: update dimn_waktu

--- src/ddl/pred_susu.sql
+++ src/ddl/pred_susu.sql
@@ -3,9 +3,9 @@ CREATE TABLE pred_susu (
   id_waktu INTEGER,
   id_lokasi INTEGER,
   id_unit_peternakan INTEGER,
-  prediction NUMERIC(10, 3),
-  latency NUMERIC(10, 10),
-  mape NUMERIC(10, 10),
+  prediction NUMERIC(20, 3),
+  latency NUMERIC(20, 10),
+  mape NUMERIC(20, 10),
   created_at TIMESTAMP,
   PRIMARY KEY (id)
 );
--- src/query/dim_waktu.sql
+++ src/query/dim_waktu.sql
@@ -1,35 +1,52 @@
-WITH cte_old_year AS (
-  SELECT
+WITH cte_dt AS (
+  SELECT dt::DATE
+    FROM GENERATE_SERIES(
+      '2000-01-01',
+      '2030-12-31',
+      INTERVAL '1 DAY'
+    ) AS dt
+),
+cte_year AS (
+  SELECT DISTINCT
     NULL::DATE AS tanggal,
-    tahun,
+    DATE_PART('year', dt)::INT AS tahun,
     NULL::INT AS bulan,
     NULL::INT AS hari
-  FROM GENERATE_SERIES(2009, 2022) AS tahun
+  FROM cte_dt
 ),
-cte_new AS (
+cte_month AS (
+  SELECT DISTINCT
+    NULL::DATE AS tanggal,
+    DATE_PART('year', dt)::INT AS tahun,
+    DATE_PART('month', dt)::INT AS bulan,
+    NULL::INT AS hari
+  FROM cte_dt
+),
+cte_day AS (
   SELECT
     dt AS tanggal,
     DATE_PART('year', dt)::INT AS tahun,
     DATE_PART('month', dt)::INT AS bulan,
     DATE_PART('day', dt)::INT AS hari
-  FROM (
-    SELECT dt::DATE
-    FROM GENERATE_SERIES(
-      '2023-01-01',
-      '2027-12-31',
-      INTERVAL '1 DAY'
-    ) AS dt
-  ) AS gen_dt
+  FROM cte_dt
 )
 SELECT
-  ROW_NUMBER() OVER (ORDER BY tanggal) AS id,
+  ROW_NUMBER() OVER (
+    ORDER BY
+      tanggal,
+      COALESCE(tahun, -1),
+      COALESCE(bulan, -1),
+      COALESCE(hari, -1)
+  ) AS id,
   tanggal,
   tahun,
   bulan,
   hari
 FROM (
-  SELECT * FROM cte_old_year
+  SELECT * FROM cte_year
   UNION ALL
-  SELECT * FROM cte_new
+  SELECT * FROM cte_month
+  UNION ALL
+  SELECT * FROM cte_day
 ) AS ud
 ORDER BY 1;
